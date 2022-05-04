664a30f76f0467583f0bbd765931e94525667fd3
ATulach <otulach@gmail.com>
2022-05-04 19:31:46 +0400

    adding CORS checks

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 const PORT = process.env.PORT || 3500;
+
+const whiteList = ["https://www.google.com"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// middleware to handle form data
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
